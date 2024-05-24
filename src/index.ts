36ada4003979103cd40e3b4a0d3c4595d1da2371
정우재 <dnwo6147@gmail.com>
2024-05-24 09:52:04 +0900

    [ADD] esm, commonjs, ts 환경 호환

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+function add(a: number, b: number) {
+  return a + b;
+}
+function subtract(a: number, b: number) {
+  return a - b;
+}
+function multiply(a: number, b: number) {
+  return a * b;
+}
+function divide(a: number, b: number) {
+  return a / b;
+}
+
+function greet() {
+  console.log("hello woojaeworld");
+}
+
+export { greet, add, subtract, multiply, divide };
